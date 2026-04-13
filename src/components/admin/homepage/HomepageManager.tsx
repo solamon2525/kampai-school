12781e2afbe532c18a008a1ee1c226e7f7b899fb
solamon2525 <toom2lyou@gmail.com>
2026-04-13 15:17:34 +0700

    fix(Admin): fix JSX syntax error in HomepageManager

--- src/components/admin/homepage/HomepageManager.tsx
+++ src/components/admin/homepage/HomepageManager.tsx
@@ -320,6 +320,7 @@ export const HomepageManager = () => {
                         activeZone={activeZone}
                     />
                 </div>
+                </div>
             </DndContext>
         </div>
     );
